6b8b47081021a0195b75048695a3f896bb34c048
TAKANO Mitsuhiro <takano32@gmail.com>
2015-04-22 16:30:34 +0900

    first commit

--- lib/lita/handlers/slack_lingr_gateway_handler.rb
+++ lib/lita/handlers/slack_lingr_gateway_handler.rb
@@ -1,6 +1,47 @@
 module Lita
   module Handlers
     class SlackLingrGatewayHandler < Handler
+      route /^(.+)$/, :slack_to_lingr
+
+      def slack_to_lingr(response)
+        say  = 'http://lingr.com/api/room/say'
+        room = 'arakawatomonori'
+        bot  = 'slack'
+        text = URI.encode([response.user.mention_name, response.matches.join].join ": ")
+        bot_verifier = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        url  = '%s?room=%s&bot=%s&text=%s&bot_verifier=%s' %
+          [say, room, bot, text, bot_verifier]
+        uri = URI.parse url
+        Net::HTTP.get(uri)
+      end
+
+      http.post '/', :pass
+      def pass(request, response)
+        require 'json'
+        req = JSON.parse request.body.read
+        events = req['events']
+        events.each do |event|
+          message = event['message']
+          room = message['room']
+          speaker_id = message['speaker_id']
+          nickname = message['nickname']
+          text = message['text']
+          puts [room, speaker_id, nickname, text]
+          puts
+          target = Lita::Source.new(room: '#general')
+          robot.send_message(target, "#{nickname}: #{text}")
+          # response.body << "#{nickname}: #{text}"
+        end
+      end
+
+      def lingr_to_slack(request, response)
+        response.body << "Hello, #{request.user_agent}!"
+      end
+
+      on(:connected) do |payload|
+        target = Source.new(room: '#general')
+        robot.send_message(target, 'Hi')
+      end
     end
 
     Lita.register_handler(SlackLingrGatewayHandler)
